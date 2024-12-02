a76a723ea9afb7f3a9c0d7bb9faeeb1d6e1549e8
Erik Lundberg <erik.lumpa@hotmail.com>
2024-12-02 10:01:24 +0100

    Add empty example file to all days

--- setup-directories.py
+++ setup-directories.py
@@ -1,13 +1,16 @@
-import os 
+import os
+
 
 def main():
-  print("Creating directories...") 
-  for day in range(1, 25):
-    os.mkdir(f'day{day}')
-    f = open(f'./day{day}/solve_part_1.py', 'x')
-    f = open(f'./day{day}/solve_part_2.py', 'x')
-    f = open(f'./day{day}/input.in', 'x')
-  print("Done!") 
+    print("Creating directories...")
+    for day in range(1, 25):
+        os.mkdir(f'day{day}')
+        f = open(f'./day{day}/solve_part_1.py', 'x')
+        f = open(f'./day{day}/solve_part_2.py', 'x')
+        f = open(f'./day{day}/input.in', 'x')
+        f = open(f'./day{day}/example.in', 'x')
+    print("Done!")
+
 
 if __name__ == '__main__':
-  main()
+    main()
